62968bcb1d54a55abdfe89683169203b3d667d6f
Templeton Tsai <qpzmy701117@gmail.com>
2015-10-11 00:38:39 +1100

    Adding plotting MDS for similarity measures

--- mds_similarity.py
+++ mds_similarity.py
@@ -19,51 +19,32 @@ import similarity_geography as sm_geo
 import similarity_industry_diversity as sm_idus_diversity
 import similarity_cultural_diversity as sm_cult_diversity
 
-#similarities = sm_idus_diversity.distance_metrix()
-similarities = sm_cult_diversity.distance_metrix()
+similarities = sm_idus_diversity.distance_metrix()
+#similarities = sm_cult_diversity.distance_metrix()
 similarities = np.array(similarities)
 similarities = similarities.reshape(34, 34)
 #np.savetxt("industry_diversity_distance_metrix.csv", similarities, delimiter=",")
-np.savetxt("cultral_diversity_distance_metrix.csv", similarities, delimiter=",")
+#np.savetxt("cultral_diversity_distance_metrix.csv", similarities, delimiter=",")
 #print similarities
 #similarities = sm_geo.init()
 #similarities = euclidean_distances(similarities)
-mds = manifold.MDS(n_components=2, max_iter=3000, eps=1e-9,
+mds = manifold.MDS(n_components=2, max_iter=5000, eps=1e-9,
 	                   dissimilarity="precomputed", n_jobs=1)
 pos = mds.fit(similarities).embedding_
+
+all_sub_features = pd.read_csv('all_sub_all_category.csv')
+sub_name = all_sub_features['Name'].tolist()
 fig = plt.figure(1)
 plt.scatter(pos[:, 0], pos[:, 1], s=30, c='g')
 sub_id =  list(range(1, 35))
 pos = pos.tolist()
 print pos
+'''
+for i, txt in enumerate(sub_name):
+    plt.annotate(txt, (pos[i][0], pos[i][1]), xytext = (-20, 20), textcoords = 'offset points', ha = 'right', va = 'bottom',
+	            bbox = dict(boxstyle = 'round,pad=0.5', fc = 'yellow', alpha = 0.5),
+		            arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3,rad=0'))
+'''
 for i, txt in enumerate(sub_id):
     plt.annotate(txt, (pos[i][0], pos[i][1]))
 plt.show()
-'''
-n_samples = 20
-seed = np.random.RandomState(seed=3)
-X_true = seed.randint(0, 20, 2 * n_samples).astype(np.float)
-print X_true
-X_true = X_true.reshape((n_samples, 2))
-print X_true
-# Center the data
-X_true -= X_true.mean()
-similarities = euclidean_distances(X_true)
-
-print similarities
-mds = manifold.MDS(n_components=2, max_iter=3000, eps=1e-9, random_state=seed,
-	                   dissimilarity="precomputed", n_jobs=1)
-pos = mds.fit(similarities).embedding_
-clf = PCA(n_components=2)
-X_true = clf.fit_transform(X_true)
-
-
-fig = plt.figure(1)
-ax = plt.axes([0., 0., 1., 1.])
-plt.scatter(X_true[:, 0], X_true[:, 1], c='r', s=20)
-plt.scatter(pos[:, 0], pos[:, 1], s=20, c='g')
-
-plt.show()
-'''
-
-
